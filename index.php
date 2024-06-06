a0d9097b377690d8b8b15e70e8b3d39025e1ecb5
CeicaIFCD0112 <rod.balseiro.ricardo@gmail.com>
2024-06-06 17:11:19 +0200

    index con post

--- index.php
+++ index.php
@@ -3,9 +3,18 @@ require 'vendor/autoload.php';
 require 'config.php';
 
 use App\Models\User;
+use App\Models\Post;
 
+/*
 $user = new User;
 $user->name = 'John Doe';
 $user->email = 'john@example.com';
 $user->password = password_hash('password', PASSWORD_BCRYPT);
 $user->save();
+*/
+$post=new Post;
+$post->tittle="Post";
+$post->body="Cuerpo post";
+$post->image="imagen.jpg";
+$post->user_id=1;
+$post->save();
