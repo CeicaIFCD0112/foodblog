b96dfc67b0d1aa3a0f0cf551596cf1b241d42ac0
CeicaIFCD0112 <rod.balseiro.ricardo@gmail.com>
2024-06-06 16:31:48 +0200

    modelos

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit249206dea31a3d30fb3de5713deb1276
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit249206dea31a3d30fb3de5713deb1276', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit249206dea31a3d30fb3de5713deb1276', 'loadClassLoader'));
@@ -30,6 +32,18 @@ class ComposerAutoloaderInit249206dea31a3d30fb3de5713deb1276
         call_user_func(\Composer\Autoload\ComposerStaticInit249206dea31a3d30fb3de5713deb1276::getInitializer($loader));
 
         $loader->register(true);
+
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit249206dea31a3d30fb3de5713deb1276::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
 
         return $loader;
     }
